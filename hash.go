9d1b01e80e1cbfeec14c1099813d9b963db4b5b6
John Kernagis <jkernagis2@gmail.com>
2017-11-19 13:56:02 -0700

    Hashing and encoding with 5s delay completed and allows multiple requests, shutdown functionality next, then clean up, tests, and README

--- hash.go
+++ hash.go
@@ -1,11 +1,18 @@
 package main
 
+import "crypto/sha512"
+import "encoding/base64"
 import "flag"
 import "fmt"
 import "log"
 import "net/http"
 import "strconv"
 import "strings"
+import "time"
+
+const(
+    SLEEP_SECONDS = 5
+)
 
 func main() {
     // Get the port to listen on
@@ -20,12 +27,29 @@ func main() {
 
 // Handles incoming requests.
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-
-    if r.Method == "GET" {
+    // Remember when we got the request so we can respond after time == SLEEP_SECONDS
+    start := time.Now()
+    if r.Method != "POST" {
         fmt.Fprintf(w, "You must post to this service")
     } else {
-        value := strings.TrimSpace(r.FormValue("value"))
+        // Do hashing and encoding right away so we can respond in as close to SLEEP_SECONDS as possible
+        password := strings.TrimSpace(r.FormValue("password"))
+        hashed := hashAndEncode(password)
         
-        fmt.Fprintf(w, "Recieved value: " + value)
-    }   
+        // Calculate remaining sleep time after calculations are done
+        remainingSleepTime := (time.Duration(SLEEP_SECONDS) * time.Second) - time.Since(start)
+        
+        // Sleep for that time, if at the time or past this will return immediately
+        time.Sleep(remainingSleepTime)
+        
+        // Return the hashed value
+        fmt.Fprintf(w, "%s", hashed)
+    }
+}
+
+// Hash and encode the input
+func hashAndEncode(s string) string {
+    sha_512 := sha512.New()
+    sha_512.Write([]byte(s))
+    return base64.StdEncoding.EncodeToString(sha_512.Sum(nil))
 }
